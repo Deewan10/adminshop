d28eedb952608f7eac3c3c6f1d8cb84807c34bd7
peterstemidayo28@gmail.com <peterstemidayo28@gmail.com>
2024-05-10 01:30:28 +0100

    more interactions

--- src/RecentOrders.js
+++ src/RecentOrders.js
@@ -19,15 +19,15 @@ function RecentOrders() {
             </tr>
           </thead>
           <tbody>
-                <OrderRow name="Floral Shirt" price="$429" payment="Paid" statusClass="inProgress" status="In Progress" />
-                <OrderRow name="Jean Short" price="$222" payment="Due" statusClass="pending" status="Pending" />
-                <OrderRow name="Vintage Shirt" price="$209" payment="Paid" statusClass="delivered" status="Delivered" />
-                <OrderRow name="Black Jacket" price="$399" payment="Paid" statusClass="return" status="Returned" />
-                <OrderRow name="Blue Shirt" price="$429" payment="Paid" statusClass="inProgress" status="In Progress" />
-                <OrderRow name="Jean Jacket" price="$522" payment="Due" statusClass="pending" status="Pending" />
-                <OrderRow name="Milk Dress" price="$79" payment="Paid" statusClass="delivered" status="Delivered" />
-                <OrderRow name="Black Hoodie" price="$399" payment="Paid" statusClass="return" status="Returned" />
-            </tbody>
+            <OrderRow name="Floral Shirt" price="$429" payment="Paid" statusClass="inProgress" status="In Progress" />
+            <OrderRow name="Jean Short" price="$222" payment="Due" statusClass="pending" status="Pending" />
+            <OrderRow name="Vintage Shirt" price="$209" payment="Paid" statusClass="delivered" status="Delivered" />
+            <OrderRow name="Black Jacket" price="$399" payment="Paid" statusClass="return" status="Returned" />
+            <OrderRow name="Blue Shirt" price="$429" payment="Paid" statusClass="inProgress" status="In Progress" />
+            <OrderRow name="Jean Jacket" price="$522" payment="Due" statusClass="pending" status="Pending" />
+            <OrderRow name="Milk Dress" price="$79" payment="Paid" statusClass="delivered" status="Delivered" />
+            <OrderRow name="Black Hoodie" price="$399" payment="Paid" statusClass="return" status="Returned" />
+         </tbody>
         </table>
       </div>
     </div>
